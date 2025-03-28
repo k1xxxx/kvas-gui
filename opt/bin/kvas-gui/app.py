6e4a4abd47d55db1a7ad51a9619391b7d8cd4479
k1xxxx <a963956@yandex.ru>
2025-03-28 12:10:14 +0300

    Debug mode is preventing the app from starting, I turned it off

--- opt/bin/kvas-gui/app.py
+++ opt/bin/kvas-gui/app.py
@@ -66,4 +66,4 @@ def vpn_status():
     return {"status": status, "color": color}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
